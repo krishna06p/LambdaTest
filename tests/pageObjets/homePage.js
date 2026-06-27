cd710748eb63000dcc190fdd31c998e0bcedae82
Krishna Patel <krishna06p@gmail.com>
2026-06-27 12:38:51 +0530

    27 June

--- tests/pageObjets/homePage.js
+++ tests/pageObjets/homePage.js
@@ -1,35 +1,25 @@
 import {page, locator} from '@playwright/test';
 
-export class homePage {
+class HomePage {
     constructor(page) {
         this.page = page;
-        this.mainBanner = page.locator('a[title="Poco Electro"]');
+        this.banner = page.locator('a[title="Poco Electro"]');
         this.navMenu = page.locator('nav-item').locator('a');
     }
 
     async navigate() {
         await this.page.goto('https://ecommerce-playground.lambdatest.io/');
-    }
-
-    async clickMainBanner() {
-        await this.mainBanner.click();
-    }   
-
-    async mainBanner() {
-        const banner = await this.mainBanner;
-        expect(banner).toBeTruthy();
     } 
 
-    async clickNavMenuItem(itemName) {
-        const menuItem = await this.navMenu.filter({ hasText: itemName });
-        await menuItem.click();
-    }
+    async mainBanner() {
+        return this.banner;
+    } 
 
     async navMenuItem(itemName) {
-        const menuItem = await this.navMenu.filter({ hasText: itemName });
-        expect(menuItem).toBeTruthy();
+        return this.navMenu.filter({ hasText: itemName });
+        //expect(menuItem).toBeTruthy();
     }
 
 }
 
-module.exports = {homePage};
+module.exports = {HomePage};
--- tests/steps/home.steps.js
+++ tests/steps/home.steps.js
@@ -1,17 +1,18 @@
 //import { Given, Then } from '@playwright-bdd/steps';
 import { expect } from '@playwright/test';
 import { createBdd } from 'playwright-bdd'; 
-import { homePage } from '../pageObjets/homePage.js';
+import { HomePage } from '../pageObjets/HomePage.js';
 
 const { Given, Then } = createBdd();
 
+let homePage;
 
 Given('I am on the home page', async ({page}) => {
-const home = new homePage(page);
+homePage = new HomePage(page);
   // Step: Given I am on the home page
   // From: features\home.feature:4:5
   //  await page.goto('https://ecommerce-playground.lambdatest.io/');
-  await home.navigate();
+  await homePage.navigate();
 });
 
 Then('the home page title should be {string}', async ({page}, arg) => {
@@ -28,7 +29,7 @@ Then('I should see the main banner', async ({page}) => {
    // const mainBanner = await page.locator('a[title="Poco Electro"]');
    
     const banner = await homePage.mainBanner();
-    expect(banner).toBeTruthy();
+    expect(banner).toBeVisible();
 });
 
 Then('I should see the navigation menu with options {string}, {string}, {string}, {string}, {string}, {string}', async ({page}, arg, arg1, arg2, arg3, arg4, arg5) => {
@@ -36,5 +37,5 @@ Then('I should see the navigation menu with options {string}, {string}, {string}
   // From: features\home.feature:13:5
     //const navMenu = await page.locator('nav-item').locator('a').filter({ hasText: arg }).first()  ;
     const navMenu = await homePage.navMenuItem(arg);
-    expect(navMenu).toBeTruthy();  
+    expect(navMenu).toBeVisible();  
 });
